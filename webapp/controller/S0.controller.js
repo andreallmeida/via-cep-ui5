c64743ab72a14ac95e9177e747ec11f0e04c5ccf
André Almeida <andre.allmeida@gmail.com>
2019-09-11 21:33:46 +0000

    Push Inicial

---
+++ webapp/controller/S0.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("alca.via-cep-ui5.controller.S0", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"alca/via-cep-ui5/test/unit/controller/S0.controller"
+], function () {
+	"use strict";
+});
